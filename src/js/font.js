b5f6a5f14de40e62944e2b066e505d9660db0c0a
John Workmeister <jtworkme@us.ibm.com>
2016-01-20 13:02:07 -0500

    first changes

---
+++ src/js/font.js
@@ -0,0 +1,11 @@
+(function ($, fluid){
+    "use strict";
+    fluid.defaults("gpii.firstDiscovery.panel.font",{
+        gradeNames: ["gpii.firstDiscovery.panel.yesNo"],
+        preferenceMap: {
+            "gpii.firstDiscovery.font": {
+                "model.value" : "default"
+            }
+        }
+    });
+})(jQuery, fluid_2_0);
